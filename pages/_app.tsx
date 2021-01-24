2776708a5e17e86cad9e40f127533c48b515c6f0
chtk3131 <takuya-ochiai@aeroasahi.co.jp>
2021-01-24 13:57:32 +0900

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import { AppProps } from 'next/app';
+import '../styles/globals.css';
+import { RecoilRoot } from 'recoil';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
+}
+
+export default MyApp;
